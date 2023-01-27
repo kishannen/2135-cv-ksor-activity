c5c788d6adc8a01e503dd451f6470200f4f9f631
kishannen <98503879+kishannen@users.noreply.github.com>
2023-01-27 17:42:27 +0800

    4 files modified, 10 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AboutComponent } from './about/about.component'
+import { EducationComponent } from './education/education.component'
+import { SkillsComponent } from './skills/skills.component'
+import { ReferencesComponent } from './references/references.component'
+import { AchievementComponent } from './achievement/achievement.component'
+
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, AboutComponent, EducationComponent, SkillsComponent, ReferencesComponent, AchievementComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
